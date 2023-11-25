e3fa8e3635c2e8b1e741f6a06d171123a6d64cda
IreneFelice <i.f.buijing@gmail.com>
2023-11-25 17:49:01 +0100

    4 articles with bag content, no component yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,48 @@
 import './App.css'
-import ButtonComponent from "./components/ButtonComponent.jsx";
-
+import ButtonComponent from "./components/ButtonComponent/ButtonComponent.jsx";
+import bag1 from './assets/bag_1.png'
+import bag2 from './assets/bag_2.png'
+import bag3 from './assets/bag_3.png'
+import bag4 from './assets/bag_4.png'
 
 function App() {
   return (
       <>
       <h1>Handbags & Purses</h1>
-        <nav>
+          <nav>
             <ButtonComponent buttonimput="to the collection" logText="to the collection" disabled={false}/>
             <ButtonComponent buttonimput="shop all bags" logText="shop all bags" disabled={false} />
             <ButtonComponent buttonimput="pre-orders" logText="pre-orders" disabled={true} />
         </nav>
 
+
           <main>
               <article>
-                  {/*<span className={}></span>*/}
-                  {/*<img src={} alt="afbeelding mist"/>*/}
-                  <p>naam tas</p>
-                  <h4>prijs</h4>
+                  <span>Best seller</span>
+                  <img src={bag1} alt="afbeelding mist"/>
+                  <p>Roze tas</p>
+                  <h4>€1000,-</h4>
               </article>
 
               <article>
-                  {/*<span className={}></span>*/}
-                  {/*<img src={} alt="afbeelding mist"/>*/}
-                  <p>naam tas</p>
-                  <h4>prijs</h4>
+                  <span>Duuuuuur!</span>
+                  <img src={bag2} alt="afbeelding mist"/>
+                  <p>Bruine tas</p>
+                  <h4>€1.000.000,-</h4>
               </article>
 
               <article>
-                  {/*<span className={}></span>*/}
-                  {/*<img src={} alt="afbeelding mist"/>*/}
-                  <p>naam tas</p>
-                  <h4>prijs</h4>
+                  <span>New collection</span>
+                  <img src={bag3} alt="afbeelding mist"/>
+                  <p>Groene tas</p>
+                  <h4>€7,50</h4>
               </article>
 
               <article>
-                  {/*<span className={}></span>*/}
-                  {/*<img src={} alt="afbeelding mist"/>*/}
-                  <p>naam tas</p>
-                  <h4>prijs</h4>
+                  <span>New collection</span>
+                  <img src={bag4} alt="afbeelding mist"/>
+                  <p>Gele tas</p>
+                  <h4>Prijs onbekend</h4>
               </article>
           </main>
   </>
--- src/components/ButtonComponent/ButtonComponent.jsx
+++ src/components/ButtonComponent/ButtonComponent.jsx
@@ -2,7 +2,6 @@ import './ButtonComponent.css'
 
 function ButtonComponent(props){
     return(
-        // eslint-disable-next-line react/prop-types
     <button type="button" disabled={props.disabled} onClick={() => console.log(props.logText)}>{props.buttonimput}</button>
 )
     }
